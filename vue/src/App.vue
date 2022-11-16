964e79efd0cb67882404bd8165ea8f62a37c8f37
ekdub92 <ekdub92@naver.com>
2022-11-16 16:29:09 +0900

    add vue

---
+++ vue/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <the-header-navbar></the-header-navbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TheHeaderNavbar from "@/components/TheHeaderNavbar";
+
+export default {
+  name: "App",
+  components: {
+    TheHeaderNavbar,
+  },
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "LeferiPoint-WhiteObliqueA";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/LeferiPoint-WhiteObliqueA.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
+
+#app {
+  font-family: "LeferiPoint-WhiteObliqueA", Avenir, Helvetica, Arial, sans-serif;
+  /* -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
+  text-align: center;
+  /* color: #2c3e50; */
+}
+
+a.router-link-exact-active {
+  color: #89bfef;
+}
+
+a.router-link-exact-active:hover {
+  text-decoration: none;
+}
+</style>
---
+++ vue/src/views/AppMain.vue
@@ -0,0 +1,38 @@
+<script setup>
+import vueMkHeader from "@/assets/main_02.png";
+</script>
+<script>
+export default {
+  name: "AppMain",
+  props: {
+    msg: String,
+  },
+};
+</script>
+<template>
+  <div>
+    <Header>
+      <div class="page-header min-vh-100" :style="`background-image: url(${vueMkHeader})`" loading="lazy">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-7 text-center mx-auto position-relative">
+              <h1 class="text-white pt-3 mt-n5 me-2" :style="{ display: 'inline-block ' }"></h1>
+              <p class="lead text-white px-5 mt-3" :style="{ fontWeight: '500' }">
+                우리가 사랑하는 곳은 집이다. 발은 떠나도 마음이 떠나지 않는 곳이 우리의 집이다. <br/><br/>
+                올리버 웬델 홈스(Oliver Wendell Holmes, 미국의 의학자, 시인)
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Header>
+  </div>
+  <!-- <b-img :src="require('@/assets/main_02.png')" fluid /> -->
+</template>
+
+<style scoped>
+/* .underline-steelblue {
+  display: inline-block;
+  background: linear-gradient(180deg, rgba(255, 255, 255, 0) 70%, rgba(72, 190, 233, 0.3) 30%);
+} */
+</style>
