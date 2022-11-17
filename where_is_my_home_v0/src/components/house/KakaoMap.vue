828a5c3ec65c12ab969da876e8ea6e743cc18374
ekdub92 <ekdub92@naver.com>
2022-11-17 17:06:24 +0900

    add tabbox

--- where_is_my_home_v0/src/components/house/KakaoMap.vue
+++ where_is_my_home_v0/src/components/house/KakaoMap.vue
@@ -1,5 +1,17 @@
 <template>
-  <div id="map"></div>
+  <div>
+    <div id="map"></div>
+    <div id="searchBox" class="card">
+      <b-tabs content-class="mt-3">
+        <b-tab title="동 검색" active>
+
+        </b-tab>
+        <b-tab title="키워드 검색">
+          
+        </b-tab>
+      </b-tabs>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -33,5 +45,19 @@ export default {
 #map {
   width: 100%;
   height: 90vh;
+}
+#searchBox {
+  position: absolute;
+  top: 20px;
+  /* bottom : 10px; */
+  left: 20px;
+  width: 400px;
+  /* height: 400px; */
+  padding: 10px;
+  z-index: 100;
+  /* background-color:rgba(255, 244, 244, 0.8); */
+  /* opacity: 0.5; */
+  background-color: rgba( 255, 255, 255, 0.7 );
+  overflow-y: auto;
 }
 </style>
