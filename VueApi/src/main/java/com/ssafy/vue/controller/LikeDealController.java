30febb94c71f1cba230c031312c0db8eef8993fb
kjyk0835 <kjyk0835@naver.com>
2022-11-21 15:41:00 +0900

    BackEnd: 찜한 매물 추가 api 구현 완료

--- VueApi/src/main/java/com/ssafy/vue/controller/LikeDealController.java
+++ VueApi/src/main/java/com/ssafy/vue/controller/LikeDealController.java
@@ -1,20 +1,31 @@
 package com.ssafy.vue.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.vue.model.LikeDealDto;
+import com.ssafy.vue.model.RegisterLikeDealParameterDto;
 import com.ssafy.vue.model.service.LikeDealService;
 
 @RequestMapping("/like-deals")
 @RestController
 public class LikeDealController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
 	
 	@Autowired
 	private LikeDealService likeDealService;
@@ -34,5 +45,14 @@ public class LikeDealController {
 			return ResponseEntity.notFound().build();
 		}
 		
+	}
+	
+	@PostMapping("/{userIdx}/{dealIdx}")
+	private ResponseEntity<?> registerLikeDeal(@PathVariable("userIdx") int userIdx, @PathVariable("dealIdx") int dealIdx) {
+		logger.info("registerLikeDeal - 호출");
+		if (likeDealService.registerLikeDeal(new RegisterLikeDealParameterDto(userIdx, dealIdx))) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
 	}
 }
--- VueApi/src/main/java/com/ssafy/vue/model/mapper/LikeDealMapper.java
+++ VueApi/src/main/java/com/ssafy/vue/model/mapper/LikeDealMapper.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.ssafy.vue.model.LikeDealDto;
+import com.ssafy.vue.model.RegisterLikeDealParameterDto;
 
 @Mapper
 public interface LikeDealMapper {
 	
 	public List<LikeDealDto> getLikeDealsByUserIdx(int userIdx);
-//	public int registerLikeDeal(LikeDealDto likeDealDto);
+	public int insertLikeDeal(RegisterLikeDealParameterDto registerLikeDealParameterDto);
 }
