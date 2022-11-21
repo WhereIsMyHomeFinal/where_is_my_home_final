4b39eb8fe4026a8631d335cb631d14e6150c37f7
kjyk0835 <kjyk0835@naver.com>
2022-11-21 10:44:33 +0900

    Front: where_is_my_home_vue 프로젝트의 코드를 where_is_my_home_v0 프로젝트에 적용 완료, css 약간 수정

--- where_is_my_home_v0/src/components/house/tabbox/KeywordSearch.vue
+++ where_is_my_home_v0/src/components/house/tabbox/KeywordSearch.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    keyword search
+    <input v-model="keyword" type="text" placeholder="건물명을 입력하세요"/>
+    <button @click="searchKeyword" class="btn btn-primary d-inline-block"><i class="fa-regular fa-magnifying-glass"></i></button>
   </div>
 </template>
 <script>
+import http from "@/api/http.js";
+
 export default {
-  
+  name: "KeywordSearch",
+  data() {
+    return {
+      keyword: "",
+    };
+  },
+  methods: {
+    searchKeyword() {
+      if (this.keyword !== "") {
+        http.get(`/house-deals?aptName=${this.keyword}`)
+        .then(({ data }) => {
+          this.$emit("searched-houses", data);
+        });
+      } else {
+        console.log("null keyword!");
+      }
+    }
+  }
 }
 </script>
 <style scoped>
